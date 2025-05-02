eb6de73b3e7e52682706a230700bb1ccb6b003a4
marelinrosal <marvantaeh@gmail.com>
2025-05-02 15:43:59 -0600

    cambio de opciones

--- pagina-amid/app/page.tsx
+++ pagina-amid/app/page.tsx
@@ -1,143 +1,270 @@
 'use client';
+import { useState } from 'react';
 import Navbar from '@/components/Navbar';
-import Intergrantes from '@/components/Integrantes';
 import Image from 'next/image';
 import '@/styles/page-m.css';
-import {Link} from "@heroui/link";
 import Card from '@/components/Cards';
-import Preguntas from '@/components/Preguntas'
-
-
-
+import Preguntas from '@/components/Preguntas';
 
 export default function Home() {
-  interface ScrollEvent extends React.MouseEvent<HTMLAnchorElement, MouseEvent> {}
 
+  const [activeSection, setActiveSection] = useState('inicio');
+  
+
+  const handleNavigation = (section: string) => {
+    setActiveSection(section);
+  };
   
   return (
     <main className="main">
+      <Navbar onNavigate={handleNavigation} />
       
-      <Navbar />
-      <div className="container w-auto mx-auto max-w-7xl pt-16 px-6 flex-grow  mt-48">
-        <Image
-          id='inicio'
-          src="/imgs/amidIcono.png"
-          alt="Logo de AMID"
-          width={300}
-          height={20}
-          className="mx-auto -mt-40 -z-10"
-        />
-        <div className='textos mt-20 font-abc z-10'>
-        <p className="text-2xl text-center z-10 animate-ty">Juntos por la inclusión, construyendo un mundo sin barreras.</p>
-        <p className="text-2xl text-center animate-typing" >Conócenos, nuestros logros y contactos</p>
+
+      {activeSection === 'inicio' && (
+        <>
+        <div className="container w-auto mx-auto max-w-7xl pt-16 px-6 flex-grow mt-48">
+          <Image
+            id='inicio'
+            src="/imgs/amidIcono.png"
+            alt="Logo de AMID"
+            width={300}
+            height={20}
+            className="mx-auto -mt-40 -z-10"
+          />
+          <div className='textos mt-20 font-abc z-10'>
+            <p className="text-2xl text-center z-10 animate-ty">Juntos por la inclusión, construyendo un mundo sin barreras.</p>
+            <p className="text-2xl text-center animate-typing">Conócenos, nuestros logros y contactos</p>
+          </div>
         </div>
         
-      </div>
-      <video src= "imgs/videofondo.mp4" autoPlay loop muted className='video'></video>
-      <div>
-      <h1 className="mt-40 ">CONOCENOS</h1>
-      <p className="text-balance text-center mt-10 font-abc w-auto text-2xl">
-      En AMID, trabajamos con pasión para transformar vidas y promover un mundo más inclusivo. 
-      Somos una asociación dedicada a apoyar a personas con discapacidad, sus familias y 
-      comunidades, ofreciendo recursos, programas y servicios que fomentan la autonomía, 
-      la dignidad y la igualdad de oportunidades.
-      </p>
-      <div className=' flex flex-row items-center mt-10 space-x-56 justify-center'>
-        <div className='flex flex-col items-center'>
-      <Image
-          id='iconos'
-          src = "/imgs/iconoObjetivos.svg"
-          alt="Logo de AMID"
-          width={150}
-          height={50}
-          className="img2 "
-        ></Image>
-        <h1 className='mt-10'>Objetivo</h1>
-       <p className='text-wrap font-abc  text-center text-lg w-80 mt-5'>
-        Empoderar a las personas con discapacidad mediante el acceso a herramientas, formación y espacios inclusivos que les permitan desarrollar su potencial y participar plenamente en la sociedad.
-        </p>
-        </div>
-        <div className='concocenos-text flex flex-col items-center'>
-    <Image
-          id='iconos'
-          src = "/imgs/iconoValores.svg"
-          alt="Logo de AMID"
-          width={150}
-          height={50}
-          className="img2  "
-        ></Image>
-        <h1 className='mt-10'>Valores</h1>
-        <p className='concocenos-text font-abc text-lg text-wrap text-center w-80 mt-5'>
-        Promovemos la inclusión, la diversidad y el respeto por los derechos humanos de todas las personas, independientemente de sus capacidades. Fomentamos la colaboración y el trabajo en equipo para lograr un impacto positivo en la comunidad.
-        </p>
-        </div>
-        <div className='flex flex-col items-center'>
-      <Image
-          id='iconos'
-          src = "/imgs/iconoVision.svg"
-          alt="Logo de AMID"
-          width={150}
-          height={50}
-          className="img2 "
-        ></Image>
-        <h1 className='mt-10'>Visión</h1>
-        <p className='concocenos-text font-abc text-balance text-lg text-center w-80 mt-5'>
-        Ser un referente en la promoción de la inclusión y el bienestar de las personas con discapacidad, trabajando en colaboración con la comunidad para construir un futuro más justo y equitativo.
-        </p>
+        <video src="imgs/videofondo.mp4" autoPlay loop muted className='video'></video>
+        
+        <div>
+          <h1 className="mt-40">CONOCENOS</h1>
+          <p className="text-balance text-center mt-10 font-abc w-auto text-2xl">
+            En AMID, trabajamos con pasión para transformar vidas y promover un mundo más inclusivo. 
+            Somos una asociación dedicada a apoyar a personas con discapacidad, sus familias y 
+            comunidades, ofreciendo recursos, programas y servicios que fomentan la autonomía, 
+            la dignidad y la igualdad de oportunidades.
+          </p>
+          <div className='flex flex-row items-center mt-10 space-x-56 justify-center'>
+            <div className='flex flex-col items-center'>
+              <Image
+                id='iconos'
+                src="/imgs/iconoObjetivos.svg"
+                alt="Logo de AMID"
+                width={150}
+                height={50}
+                className="img2"
+              />
+              <h1 className='mt-10'>Objetivo</h1>
+              <p className='text-wrap font-abc text-center text-lg w-80 mt-5'>
+                Empoderar a las personas con discapacidad mediante el acceso a herramientas, formación y espacios inclusivos que les permitan desarrollar su potencial y participar plenamente en la sociedad.
+              </p>
+            </div>
+            <div className='concocenos-text flex flex-col items-center'>
+              <Image
+                id='iconos'
+                src="/imgs/iconoValores.svg"
+                alt="Logo de AMID"
+                width={150}
+                height={50}
+                className="img2"
+              />
+              <h1 className='mt-10'>Valores</h1>
+              <p className='concocenos-text font-abc text-lg text-wrap text-center w-80 mt-5'>
+                Promovemos la inclusión, la diversidad y el respeto por los derechos humanos de todas las personas, independientemente de sus capacidades. Fomentamos la colaboración y el trabajo en equipo para lograr un impacto positivo en la comunidad.
+              </p>
+            </div>
+            <div className='flex flex-col items-center'>
+              <Image
+                id='iconos'
+                src="/imgs/iconoVision.svg"
+                alt="Logo de AMID"
+                width={150}
+                height={50}
+                className="img2"
+              />
+              <h1 className='mt-10'>Visión</h1>
+              <p className='concocenos-text font-abc text-balance text-lg text-center w-80 mt-5'>
+                Ser un referente en la promoción de la inclusión y el bienestar de las personas con discapacidad, trabajando en colaboración con la comunidad para construir un futuro más justo y equitativo.
+              </p>
+            </div>
+          </div>
         </div>
         
+        <div id="miembros" className="mt-10">
+          <h1 className="mt-10">Miembros</h1>
+          <p className="text-center text-balance mt-10 font-abc w-screen text-2xl">
+            En AMID, contamos con un equipo diverso y comprometido que trabaja incansablemente para promover la inclusión y el bienestar de las personas con discapacidad. Nuestros miembros son apasionados defensores de los derechos humanos y están dedicados a crear un mundo más justo y equitativo para todos.
+          </p>
+          <div className='flex lg:flex-raw items-center mt-10 space-x-56 justify-center'>
+            <Card
+              cargo='Socio Fundador'
+              estudio='Estudiante de Derecho'
+              nombre='Hector Javier'
+              url='https://i.pravatar.cc/150?u=a042581f4e29026024d'
+            />
+            <Card
+              cargo='Tesorera'
+              estudio='Licenciada en Psicología'
+              nombre='Yetmari Carmona'
+              url='https://i.pravatar.cc/150?u=a042581f4e29026704d'
+            />
+            <Card
+              cargo='Coordinadora de Proyectos'
+              estudio='Licenciada en Educación Especial'
+              nombre='Zoey Lang'
+              url='https://i.pravatar.cc/150?u=a04258114e29026702d'
+            />
+            <Card
+              cargo='Coordinadora de Comunicación'
+              estudio='Licenciada en Comunicación'
+              nombre='Zoey Lang'
+              url='https://i.pravatar.cc/150?u=a04258114e29026702d'
+            />
+            <Card
+              cargo='Coordinadora de Eventos'
+              estudio='Licenciada en Administración'
+              nombre='Zoey Lang'
+              url='https://i.pravatar.cc/150?u=a04258114e29026702d'
+            />
+          </div>
+        </div>
         
-      </div>
+        <div>
+          <Preguntas />
+        </div>
+      </>
+    )}
       
-      </div>
-      <div>
 
-      <h1 className="mt-10">Miembros</h1>
-      <p className=" text-center text-balance mt-10 font-abc w-screen text-2xl">
-      En AMID, contamos con un equipo diverso y comprometido que trabaja incansablemente para promover la inclusión y el bienestar de las personas con discapacidad. Nuestros miembros son apasionados defensores de los derechos humanos y están dedicados a crear un mundo más justo y equitativo para todos.
-      </p>
-      <div className='flex lg:flex-raw items-center mt-10 space-x-56 justify-center'>
-      <Card
-      cargo='Socio Fundador'
-      estudio='Estudiante de Derecho'
-      nombre='Hector Javier'
-      url='https://i.pravatar.cc/150?u=a042581f4e29026024d'
-      />
-      <Card
-      cargo='Tesorera'
-      estudio='Licenciada en Psicología'
-      nombre='Yetmari Carmona'
-      url='https://i.pravatar.cc/150?u=a042581f4e29026704d'
-      />
-      <Card
-      cargo='Coordinadora de Proyectos'
-      estudio='Licenciada en Educación Especial'
-      nombre='Zoey Lang'
-      url='https://i.pravatar.cc/150?u=a04258114e29026702d'
-      />
-      <Card
-      cargo='Coordinadora de Comunicación'
-      estudio='Licenciada en Comunicación'
-      nombre='Zoey Lang'
-      url='https://i.pravatar.cc/150?u=a04258114e29026702d'
-      />
-      <Card
-      cargo='Coordinadora de Eventos'
-      estudio='Licenciada en Administración'
-      nombre='Zoey Lang'
-      url='https://i.pravatar.cc/150?u=a04258114e29026702d'
-      />
-      </div>
-      </div>
+      {activeSection === 'conocenos' && (
+        <div className="mt-32">
+          <h1 className="mt-40">CONOCENOS</h1>
+          <p className="text-balance text-center mt-10 font-abc w-auto text-2xl">
+            En AMID, trabajamos con pasión para transformar vidas y promover un mundo más inclusivo. 
+            Somos una asociación dedicada a apoyar a personas con discapacidad, sus familias y 
+            comunidades, ofreciendo recursos, programas y servicios que fomentan la autonomía, 
+            la dignidad y la igualdad de oportunidades.
+          </p>
+          <div className='flex flex-row items-center mt-10 space-x-56 justify-center'>
+            <div className='flex flex-col items-center'>
+              <Image
+                id='iconos'
+                src="/imgs/iconoObjetivos.svg"
+                alt="Logo de AMID"
+                width={150}
+                height={50}
+                className="img2"
+              />
+              <h1 className='mt-10'>Objetivo</h1>
+              <p className='text-wrap font-abc text-center text-lg w-80 mt-5'>
+                Empoderar a las personas con discapacidad mediante el acceso a herramientas, formación y espacios inclusivos que les permitan desarrollar su potencial y participar plenamente en la sociedad.
+              </p>
+            </div>
+            <div className='concocenos-text flex flex-col items-center'>
+              <Image
+                id='iconos'
+                src="/imgs/iconoValores.svg"
+                alt="Logo de AMID"
+                width={150}
+                height={50}
+                className="img2"
+              />
+              <h1 className='mt-10'>Valores</h1>
+              <p className='concocenos-text font-abc text-lg text-wrap text-center w-80 mt-5'>
+                Promovemos la inclusión, la diversidad y el respeto por los derechos humanos de todas las personas, independientemente de sus capacidades. Fomentamos la colaboración y el trabajo en equipo para lograr un impacto positivo en la comunidad.
+              </p>
+            </div>
+            <div className='flex flex-col items-center'>
+              <Image
+                id='iconos'
+                src="/imgs/iconoVision.svg"
+                alt="Logo de AMID"
+                width={150}
+                height={50}
+                className="img2"
+              />
+              <h1 className='mt-10'>Visión</h1>
+              <p className='concocenos-text font-abc text-balance text-lg text-center w-80 mt-5'>
+                Ser un referente en la promoción de la inclusión y el bienestar de las personas con discapacidad, trabajando en colaboración con la comunidad para construir un futuro más justo y equitativo.
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
 
-      <div>
-      <h1 className="mt-10">Preguntas Frecuentes</h1>
-        <Preguntas/>
-      </div>
-     
+      {activeSection === 'comisiones' && (
+        <div className="mt-32">
+          <h1 className="mt-10">Comisiones</h1>
+          <p className="text-center text-balance mt-10 font-abc w-screen text-2xl">
+            
+          </p>
+        </div>
+      )}
+      
+      {activeSection === 'proyectos' && (
+        <div className="mt-32">
+          <h1 className="mt-10">Proyectos</h1>
+          <p className="text-center text-balance mt-10 font-abc w-screen text-2xl">
+            
+          </p>
+        </div>
+      )}
 
-     </main>
-    
 
+      {activeSection === 'preguntas' && (
+        <div className="mt-32">
+          <h1 className="mt-10">Preguntas frecuentes</h1>
+          <Preguntas />
+        </div>
+      )}
+
+
+      {activeSection === 'miembros' && (
+        <div id="miembros" className="mt-32">
+          <h1 className="mt-10">Miembros</h1>
+          <p className="text-center text-balance mt-10 font-abc w-screen text-2xl">
+            En AMID, contamos con un equipo diverso y comprometido que trabaja incansablemente para promover la inclusión y el bienestar de las personas con discapacidad. Nuestros miembros son apasionados defensores de los derechos humanos y están dedicados a crear un mundo más justo y equitativo para todos.
+          </p>
+          <div className='flex lg:flex-raw items-center mt-10 space-x-56 justify-center'>
+            <Card
+              cargo='Socio Fundador'
+              estudio='Estudiante de Derecho'
+              nombre='Hector Javier'
+              url='https://i.pravatar.cc/150?u=a042581f4e29026024d'
+            />
+            <Card
+              cargo='Tesorera'
+              estudio='Licenciada en Psicología'
+              nombre='Yetmari Carmona'
+              url='https://i.pravatar.cc/150?u=a042581f4e29026704d'
+            />
+            <Card
+              cargo='Coordinadora de Proyectos'
+              estudio='Licenciada en Educación Especial'
+              nombre='Zoey Lang'
+              url='https://i.pravatar.cc/150?u=a04258114e29026702d'
+            />
+            <Card
+              cargo='Coordinadora de Comunicación'
+              estudio='Licenciada en Comunicación'
+              nombre='Zoey Lang'
+              url='https://i.pravatar.cc/150?u=a04258114e29026702d'
+            />
+            <Card
+              cargo='Coordinadora de Eventos'
+              estudio='Licenciada en Administración'
+              nombre='Zoey Lang'
+              url='https://i.pravatar.cc/150?u=a04258114e29026702d'
+            />
+          </div>
+        </div>
+      )}
+      
+
+      
+    </main>
   );
 }
--- pagina-amid/components/Navbar.tsx
+++ pagina-amid/components/Navbar.tsx
@@ -1,53 +1,51 @@
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@heroui/react";
 import Image from "next/image";
 
-
-  
-
-export default function App() {
+export default function App({ onNavigate }: { onNavigate: (section: string) => void }){
   return (
     <Navbar shouldHideOnScroll className="bg-sky-50 fixed top-0 left-0 h-28" >
       <NavbarBrand>
-      
-        <p className="font-bold text-inherit  text-2xl">
-           <Image
-                    id='inicio'
-                    src="/imgs/amidLogo.png"
-                    alt="Logo de AMID"
-                    width={200}
-                    height={100}
-                    className="mx-auto mt-0 -z-10 "
-                  />
+        <p className="font-bold text-inherit text-2xl">
+          <Image
+            id='inicio'
+            src="/imgs/amidLogo.png"
+            alt="Logo de AMID"
+            width={200}
+            height={100}
+            className="mx-auto mt-0 -z-10"
+            onClick={() => onNavigate('inicio')}
+            style={{cursor: 'pointer'}}
+          />
         </p>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-6 " justify="center">
+      <NavbarContent className="hidden sm:flex gap-6" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
-          Conócenos
+          <Link color="foreground" href="#" onClick={() => onNavigate('conocenos')}>
+            Conócenos
           </Link>
         </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="#conocenos" >
+        <NavbarItem>
+          <Link color="foreground" href="#" onClick={() => onNavigate('comisiones')}>
             Comisiones
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="#" onClick={() => onNavigate('proyectos')}>
             Proyectos
           </Link>
         </NavbarItem>
         <NavbarItem>
-          Preguntas Frecuentes
+          <Link color="foreground" href="#" onClick={() => onNavigate('preguntas')}>
+            Preguntas Frecuentes
+          </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="#" onClick={() => onNavigate('miembros')}>
             Miembros
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        
-        
       </NavbarContent>
     </Navbar>
   );
