8682fdc7ea29ea4883b7e3722b49df249c4b9df9
IsraelJi <L22281378@toluca.tecnm.mx>
2025-05-14 09:47:45 -0600

    cOMMIT

--- pagina-amid/next.config.js
+++ pagina-amid/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export'
+};
+
+
 
 module.exports = nextConfig;
-
-const withVideos = require('next-videos')
-
-module.exports = withVideos()
